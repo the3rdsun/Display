d137ea70ad2d628a063e2423055f194ee4c521c6
Kyle Francis <kfminiman5@googlemail.com>
2014-06-23 16:14:32 +0100

    Added functions for digits 1 - 9

--- display4/display4.ino
+++ display4/display4.ino
@@ -18,6 +18,22 @@ int CM4 = 13;
 int delayOn = 1500;
 int delayOff = 500;
 
+void allOff() {
+  digitalWrite(ledA, OFF);
+  digitalWrite(ledB, OFF);
+  digitalWrite(ledC, OFF);
+  digitalWrite(ledD, OFF);
+  digitalWrite(ledE, OFF);
+  digitalWrite(ledF, OFF);
+  digitalWrite(ledG, OFF);
+  digitalWrite(ledDP, OFF);
+  
+  digitalWrite(CM1, LOW);
+  digitalWrite(CM2, LOW);
+  digitalWrite(CM3, LOW);
+  digitalWrite(CM4, LOW);
+}
+
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
@@ -35,76 +51,136 @@ void setup() {
   pinMode(CM3, OUTPUT);
   pinMode(CM4, OUTPUT);
   
-  digitalWrite(ledA, OFF);
-  digitalWrite(ledB, OFF);
-  digitalWrite(ledC, OFF);
-  digitalWrite(ledD, OFF);
-  digitalWrite(ledE, OFF);
-  digitalWrite(ledF, OFF);
-  digitalWrite(ledG, OFF);
-  digitalWrite(ledDP, OFF);
+  allOff();
+ 
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  //Display pattern 1
+void display0() {
+  digitalWrite(ledA, ON);
   digitalWrite(ledB, ON);
   digitalWrite(ledC, ON);
-  digitalWrite(CM1, HIGH);
+  digitalWrite(ledD, ON);
+  digitalWrite(ledE, ON);
+  digitalWrite(ledF, ON);
+  delayMicroseconds(500);
+ 
+}
+
+void display1() {
+  digitalWrite(ledB, ON);
+  digitalWrite(ledC, ON);
   delayMicroseconds(500);
   
-  //Turn it off
-  digitalWrite(ledB, OFF);
-  digitalWrite(ledC, OFF);
-  digitalWrite(CM1, LOW);
-  
-  //Display pattern 2
+}
+
+void display2() {
   digitalWrite(ledA, ON);
   digitalWrite(ledB, ON);
   digitalWrite(ledD, ON);
   digitalWrite(ledE, ON);
   digitalWrite(ledG, ON);
-  digitalWrite(CM2, HIGH);
   delayMicroseconds(500);
   
-  //Turn it off
-  digitalWrite(ledA, OFF);
-  digitalWrite(ledB, OFF);
-  digitalWrite(ledD, OFF);
-  digitalWrite(ledE, OFF);
-  digitalWrite(ledG, OFF);
-  digitalWrite(CM2, LOW);
-  
-  //Display pattern 3
+}
+
+void display3() {
   digitalWrite(ledA, ON);
   digitalWrite(ledB, ON);
   digitalWrite(ledC, ON);
   digitalWrite(ledD, ON);
   digitalWrite(ledG, ON);
-  digitalWrite(CM3, HIGH);
   delayMicroseconds(500);
-  
-  //Turn it off
-  digitalWrite(ledA, OFF);
-  digitalWrite(ledB, OFF);
-  digitalWrite(ledC, OFF);
-  digitalWrite(ledD, OFF);
-  digitalWrite(ledG, OFF);
-  digitalWrite(CM3, LOW);
-  
-  //Display pattern 4
+
+}
+
+void display4() {
   digitalWrite(ledB, ON);
   digitalWrite(ledC, ON);
   digitalWrite(ledF, ON);
   digitalWrite(ledG, ON);
-  digitalWrite(CM4, HIGH);
   delayMicroseconds(500);
+
+}
+
+void display5() {
+  digitalWrite(ledA, ON);
+  digitalWrite(ledC, ON);
+  digitalWrite(ledD, ON);
+  digitalWrite(ledF, ON);
+  digitalWrite(ledG, ON);
+  delayMicroseconds(500);
+
+}
+
+void display6() {
+  digitalWrite(ledA, ON);
+  digitalWrite(ledC, ON);
+  digitalWrite(ledD, ON);
+  digitalWrite(ledE, ON);
+  digitalWrite(ledF, ON);
+  digitalWrite(ledG, ON);
+  delayMicroseconds(500);
+
+}
+
+void display7() {
+  digitalWrite(ledA, ON);
+  digitalWrite(ledB, ON);
+  digitalWrite(ledC, ON);
+  delayMicroseconds(500);
+
+}
+
+void display8() {
+  digitalWrite(ledA, ON);
+  digitalWrite(ledB, ON);
+  digitalWrite(ledC, ON);
+  digitalWrite(ledD, ON);
+  digitalWrite(ledE, ON);
+  digitalWrite(ledF, ON);
+  digitalWrite(ledG, ON);
+  delayMicroseconds(500);
+
+}
+
+void display9() {
+  digitalWrite(ledA, ON);
+  digitalWrite(ledB, ON);
+  digitalWrite(ledC, ON);
+  digitalWrite(ledF, ON);
+  digitalWrite(ledG, ON);
+  delayMicroseconds(500);
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  //Display pattern 1
+  digitalWrite(CM1, HIGH);
+  display5();
   
   //Turn it off
-  digitalWrite(ledB, OFF);
-  digitalWrite(ledC, OFF);
-  digitalWrite(ledF, OFF);
-  digitalWrite(ledG, OFF);
-  digitalWrite(CM4, LOW);
+  allOff();
+  
+  //Display pattern 2
+  digitalWrite(CM2, HIGH);
+  display6();
+  
+  //Turn it off
+  allOff();
+  
+  //Display pattern 3
+  digitalWrite(CM3, HIGH);
+  display7();
+  
+  //Turn it off
+  allOff();
+  
+  //Display pattern 4
+  digitalWrite(CM4, HIGH);
+  display8();
+  
+  //Turn it off
+  allOff();
   
 }
