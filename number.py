372c865aa5f1da6d46dc0118f60cbd39626cb778
Kyle <kfminiman5@googlemail.com>
2014-06-25 09:11:24 +0000

    Added clock program to display minutes and seconds on display

--- number.py
+++ number.py
@@ -1,5 +1,14 @@
 import RPi.GPIO as GPIO
 import serial, time
+import sys
+
+print(" ")
+print("Numbers must be between 9999 and 0!")
+print(" ")
+
+startNum = int(raw_input("Enter your start number!: "))
+endNum = int(raw_input("Enter your end number!: "))
+print(" ")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(9, GPIO.IN) #MISO
@@ -8,6 +17,24 @@ GPIO.setup(11, GPIO.IN) #SCLK
 
 ser = serial.Serial('/dev/ttyAMA0', 9600)
 
-ser.write('1234')
+def display(num) :
+	if len(str(num)) > 4 :
+		print("Number must be between 1000 and 0!")
+		sys.exit()
+
+	if len(str(num)) == 3 :
+		ser.write(' ')
+
+	if len(str(num)) == 2 :
+		ser.write('  ')
+
+	if len(str(num)) == 1 :
+		ser.write('   ')
+
+	ser.write(str(num))
+
+for n in range(startNum, endNum - 1, -1) :
+	display(n)
+	time.sleep(0.01)
 
 ser.close()
